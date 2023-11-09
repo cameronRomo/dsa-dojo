27b94f221a26cbc838563158bd601811ad00d6ed
Cameron Romo <cromo1225@gmail.com>
2023-11-09 08:44:41 -0700

    Add Stack with push method

--- stacks/lib/stack.rb
+++ stacks/lib/stack.rb
@@ -1,3 +1,31 @@
-class Stack
+class Node
+  attr_accessor :value, :next
 
+  def initialize value
+    @value = value
+    @next = nil
+  end
+end
+
+class Stack
+  attr_reader :count
+
+  def initialize()
+    @first = nil
+    @last = nil
+    @count = 0
+  end
+
+  def push value
+    new_node = Node.new value
+    if @first.nil?
+      @first = new_node
+      @last = new_node
+    else
+      @first.next = new_node
+      @first = new_node
+    end
+
+    @count += 1
+  end
 end
